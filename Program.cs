8d3c0503acfb4b307e11222815f7503bce45fe23
Rails47 <158601407+Rails47@users.noreply.github.com>
2024-04-06 19:48:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("192.168.100.3");
+            int port = 12345;
+
+            UdpClient udpClient = new UdpClient();
+
+            try
+            {
+                udpClient.Connect(ipAddress, port);
+
+                string ingredients = "яйца";
+                byte[] request = Encoding.UTF8.GetBytes(ingredients);
+                udpClient.Send(request, request.Length);
+
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] response = udpClient.Receive(ref serverEndPoint);
+                string recipes = Encoding.UTF8.GetString(response);
+                Console.WriteLine($"Рецепты с {ingredients}:");
+                Console.WriteLine(recipes);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+        }
+    }
+}
